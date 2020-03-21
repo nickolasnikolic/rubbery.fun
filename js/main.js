d4a869a97155fa74a87f5d82494cfbdc9823ce7f
nickolas nikolic <nn@nick.cool>
2020-03-21 17:13:21 -0500

    added js

--- js/main.js
+++ js/main.js
@@ -10,7 +10,13 @@ request.open('get', './api.php');
 request.send();
 
 //search and replace words with spans
-text.replace(/test/g, '<span class="">\1</span>' );
+text.replace(/idea/g, '<span class="idea">\1</span>' );
+text.replace(/concept/g, '<span class="concept">\1</span>' );
+
+//place text in target div
+let wiki = document.getElementById('wiki');
+wiki.innerHTML = text;
+
 //animate in whole sections
 //animate in color and selected text
 
